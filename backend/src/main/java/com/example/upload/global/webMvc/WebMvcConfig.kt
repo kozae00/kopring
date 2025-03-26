ccd7aa6c1961e1df1aed5031a20ea80d1dc2233c
kozae00 <rhwogus777@gmail.com>
2025-03-26 11:20:30 +0900

    refactor: WebMvcConfig

---
+++ backend/src/main/java/com/example/upload/global/webMvc/WebMvcConfig.kt
@@ -0,0 +1,14 @@
+package com.example.upload.global.webMvc
+
+import com.example.upload.global.app.AppConfig.Companion.getGenFileDirPath
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/gen/**")
+            .addResourceLocations("file:///" + getGenFileDirPath() + "/")
+    }
+}
